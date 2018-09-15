098923ef04988ce85bd658b13f78764d42fcc9a9
Nurit Levi <nuritlh@gmail.com>
2018-09-15 16:05:01 +0300

    first commit

---
+++ dist/precache-manifest.7c3a60291b5567869619f5ec3307da05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c862f565a4e19b7aec38",
+    "url": "/js/chunk-vendors.59645627.js"
+  },
+  {
+    "revision": "43a2553bb76f6d8bd3b0",
+    "url": "/js/app.a428a181.js"
+  },
+  {
+    "revision": "f9c610c49a8f49e5d3b2defeabb827bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c19dec08a9de5297d83d37061e99c89b",
+    "url": "/img/nurit.c19dec08.jpg"
+  },
+  {
+    "revision": "43a2553bb76f6d8bd3b0",
+    "url": "/css/app.d74fe19c.css"
+  }
+];
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,109 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="cover-pic screen" id="home">
+          <div class="nav">
+            <a href="#">home</a> | 
+            <a href="#professional">professional</a> | 
+            <a href="#experience">experience</a> | 
+            <a href="#protfolio">protfolio</a> | 
+            <a href="#contact">contact</a>
+          </div>
+          <p class="title">I'm</p>
+          <p class="title">nurit</p>
+          <p class="title">Levi</p>
+          <p class="title job">Full Stack Developer</p>
+    </div>
+      <div id="professional" class="page">professional
+        <i @click="scrollUp" class="fas fa-arrow-alt-circle-up"></i>
+      </div>
+      <div id="experience" class="page">experience
+        <i @click="scrollUp" class="fas fa-arrow-alt-circle-up"></i>
+      </div>
+      <div id="protfolio" class="page">protfolio
+        <i @click="scrollUp" class="fas fa-arrow-alt-circle-up"></i>
+      </div>
+      <div id="contact" class="page">contact
+        <i @click="scrollUp" class="fas fa-arrow-alt-circle-up"></i>
+      </div>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  components: {},
+  methods: {
+    scrollUp() {
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
   }
-}
+};
 </script>
+
+<style scope>
+.screen {
+  position: relative;
+}
+.screen::before {
+  content: '';
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  z-index: -1;
+  background: linear-gradient(
+    rgba(0, 0, 0, 0),
+    rgba(0, 0, 0, 0.3),
+    rgba(27, 77, 12, 0.75)
+  );
+}
+.cover-pic {
+  background-image: url('../assets/img/nurit.jpg');
+  width: 100%;
+  height: calc(100vh);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  transition: all 1s ease;
+  background-position: center, center;
+  z-index: 3;
+}
+.nav {
+  color: #ffffff;
+  position: absolute;
+  top: 0;
+  width: 100%;
+}
+.nav a {
+  color: #ffffff;
+  text-transform: uppercase;
+  font-size: 0.8em;
+  text-decoration: none;
+  padding: 0 10px;
+}
+.title {
+  color: #ffffff;
+  text-transform: uppercase;
+  font-weight: bolder;
+  font-size: 4em;
+  margin: 10px 10px 10px 100px;
+  text-align: left;
+}
+.job {
+  font-size: 1.5em;
+  font-weight: normal;
+}
+
+.page {
+  height: calc(100vh);
+}
+</style>
